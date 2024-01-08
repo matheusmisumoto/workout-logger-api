fb8aad5c3b3f09ff7e4dc6b3025539966cdb2862
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2024-01-08 01:23:38 -0300

    Fix: Validation of workout log
    - Add the @NotEmpty validation on WorkoutRecordDTO to prevent workout
      logs with no exercises
    - Update SpringBoot version to 3.2.1
    - Update API version to 1.0.1

--- src/main/java/dev/matheusmisumoto/workoutloggerapi/dto/WorkoutRecordDTO.java
+++ src/main/java/dev/matheusmisumoto/workoutloggerapi/dto/WorkoutRecordDTO.java
@@ -3,8 +3,9 @@ package dev.matheusmisumoto.workoutloggerapi.dto;
 import java.util.List;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
-public record WorkoutRecordDTO(String name, String comment, @NotNull int duration, @NotBlank String status, List<WorkoutExerciseRecordDTO> exercises) {
+public record WorkoutRecordDTO(String name, String comment, @NotNull int duration, @NotBlank String status, @NotEmpty List<WorkoutExerciseRecordDTO> exercises) {
 
 }
